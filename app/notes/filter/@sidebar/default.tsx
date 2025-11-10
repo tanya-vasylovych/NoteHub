789c916c30077c0a483ef865206b3020513897d0
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-11-10 12:14:35 +0200

    side bar

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import css from "./SidebarNotes.module.css";
+import { usePathname } from "next/navigation";
 
 const tagsList: string[] = [
   "All notes",
@@ -11,15 +13,25 @@ const tagsList: string[] = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <ul className={css.menuList}>
-      {tagsList.map((item) => (
-        <li className={css.menuItem} key={item}>
-          <Link href={`/notes/filter/${item}`} className={css.menuLink}>
-            {item}
-          </Link>
-        </li>
-      ))}
+      {tagsList.map((item) => {
+        const linkPath = `/notes/filter/${item}`;
+        const isActive = pathname === linkPath;
+
+        return (
+          <li className={css.menuItem} key={item}>
+            <Link
+              href={linkPath}
+              className={`${css.menuLink} ${isActive ? css.active : ""}`}
+            >
+              {item}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
